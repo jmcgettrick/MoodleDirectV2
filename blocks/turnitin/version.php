e3d6c47b219ef0bab2ea7e5279c723b91395cb7c
John McGettrick <jmcgettrick@iparadigms.com>
2014-02-12 10:27:25 +0000

    Dependencies added to block and plagiarism plugin so they now require mod_turnitintooltwo to work. Function to check if turnitintooltwo exists has been removed from plagiarism plugin.

--- blocks/turnitin/version.php
+++ blocks/turnitin/version.php
@@ -2,3 +2,5 @@
 
 $plugin->version = 2014012401;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2012062500; // YYYYMMDDHH (This is the release version for Moodle 2.3)
+
+$plugin->dependencies = array('mod_turnitintooltwo' => 2014012401);
