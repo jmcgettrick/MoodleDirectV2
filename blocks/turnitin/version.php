98b216ae81e25d27812745bc8f1144c1123f7d54
John McGettrick <jmcgettrick@iparadigms.com>
2014-02-14 10:11:22 +0000

    Version incremented and changed to stable. Some erroneous &#34;s fixed in lang files. center condition removed from css as it was breaking some moodle themes.

--- blocks/turnitin/version.php
+++ blocks/turnitin/version.php
@@ -1,6 +1,7 @@
 <?php
 
-$plugin->version = 2014012401;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2014012402;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2012062500; // YYYYMMDDHH (This is the release version for Moodle 2.3)
 
-$plugin->dependencies = array('mod_turnitintooltwo' => 2014012401);
+$module->maturity  = MATURITY_STABLE;
+$plugin->dependencies = array('mod_turnitintooltwo' => 2014012402);
