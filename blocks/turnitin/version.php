5451f4b51a067fa43ce94825763326c602cbcdb6
John McGettrick <jmcgettrick@iparadigms.com>
2014-03-11 16:44:48 +0000

    Release and maturity added to the version.php files for each element. Readme file also added for each individual element.

--- blocks/turnitin/version.php
+++ blocks/turnitin/version.php
@@ -2,6 +2,6 @@
 
 $plugin->version = 2014012402;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2012062500; // YYYYMMDDHH (This is the release version for Moodle 2.3)
-
-$module->maturity  = MATURITY_STABLE;
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release  = '2.3+';
 $plugin->dependencies = array('mod_turnitintooltwo' => 2014012402);
