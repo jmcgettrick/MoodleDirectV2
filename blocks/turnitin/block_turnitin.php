ab642f97bf0235e6c5cb60daaf7c9500f8405ff5
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-06-11 10:30:18 +0100

    Use M.cfg.wwwroot rather than site_root

--- blocks/turnitin/block_turnitin.php
+++ blocks/turnitin/block_turnitin.php
@@ -25,7 +25,6 @@ class block_turnitin extends block_base {
 	        $cssurl = new moodle_url($CFG->wwwroot.'/mod/turnitintooltwo/css/styles_block.css');
         	$PAGE->requires->css($cssurl);
 
-	        $output .= html_writer::script('var site_root = "'.$CFG->wwwroot.'";');
 	        $output .= $OUTPUT->box($OUTPUT->pix_icon('loader', '', 'mod_turnitintooltwo'), 'centered_cell', 'block_loading');
 	        $output .= html_writer::link($CFG->wwwroot.'/mod/turnitintooltwo/extras.php?cmd=courses',
 	        							html_writer::tag('noscript', get_string('coursestomigrate', 'mod_turnitintooltwo', '')), array('id' => 'block_migrate_content'));
