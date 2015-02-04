55959bdb616f72dabc8fe6565e4fc6d6215bbdbe
John McGettrick <jmcgettrick@iparadigms.com>
2015-02-04 11:32:49 +0000

    Block removed.

--- blocks/turnitin/version.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$plugin->version = 2014012407;  // YYYYMMDDHH (year, month, day, 24-hr time)
-$plugin->requires = 2012062500; // YYYYMMDDHH (This is the release version for Moodle 2.3)
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->release  = '2.3+';
-$plugin->dependencies = array('mod_turnitintooltwo' => 2014012407);
